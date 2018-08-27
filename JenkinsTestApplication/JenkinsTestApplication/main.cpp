f6c1ff23a8f5cdffc8e164227cf173c526e1f072
KulturBolschewist <42722269+KulturBolschewist@users.noreply.github.com>
2018-08-27 15:54:36 +0200

    testStuff

--- JenkinsTestApplication/JenkinsTestApplication/main.cpp
+++ JenkinsTestApplication/JenkinsTestApplication/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 
+#include <gtest/gtest.h>
+#include "c1.h"
 using namespace std;
 
-int main() {
 
-	cout << "Erster Test\n";
+TEST(set1, works) {
+	c1 Test_class;
+
+	Test_class.adder(17);
+
+	EXPECT_EQ(Test_class.get(), 17);
+
+}
+
+TEST(set1, fails) {
+	c1 Test_class;
+
+	Test_class.adder(27);
+
+	EXPECT_EQ(Test_class.get(), 17);
+
+}
+
+int main(int argc, char* argv[]) {
+	testing::InitGoogleTest(&argc , argv);
+	cout << RUN_ALL_TESTS();
 
 	system("PAUSE");
 
