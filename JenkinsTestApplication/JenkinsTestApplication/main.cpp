c0afc7be86a565fe1497dd840259c57433d6b1ce
KulturBolschewist <42722269+KulturBolschewist@users.noreply.github.com>
2018-08-27 15:44:32 +0200

    Revert "newStuff"
    This reverts commit 45f9a54f974691f905bae69cf042fcd96b320291.

--- JenkinsTestApplication/JenkinsTestApplication/main.cpp
+++ JenkinsTestApplication/JenkinsTestApplication/main.cpp
@@ -1,30 +1,11 @@
 #include <iostream>
 
-//#include "tester.h"
-
-#include <gtest\gtest.h>
-
-#include "c1.h"
-
 using namespace std;
 
+int main() {
 
-TEST(t1, demo) {
-	c1 Test;
-	Test.adder(17);
-	EXPECT_EQ(17, Test.get());
-}
+	cout << "Erster Test\n";
 
-TEST(t1, fail) {
-	c1 Test;
-	Test.adder(27);
-	EXPECT_EQ(17, Test.get());
-}
-
-int main(int argc, char* argv[]) {
-
-	testing::InitGoogleTest(& argc, argv);
-	cout << RUN_ALL_TESTS();
 	system("PAUSE");
 
 	return 0;
